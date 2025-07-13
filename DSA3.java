00a4b9d0bc66b63362137ffed4fd4158042c954b
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 14:14:18 +0530

    Add files via upload

---
+++ DSA3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class DSA3
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number");
+ 		int num1 = sc.nextInt();
+	 	System.out.println("number 1 is :" + num1);
+		System.out.println("Enter 2nd your number");
+		int num2 = sc.nextInt();
+		System.out.println("number 2 is :" + num2);
+		int divide = num1 / num2;
+		System.out.println("divide :" + divide);
+		
+		
+	}
+}
+
