999b38f0898c095397fca522d434bb56ad6d63a9
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 17:27:29 +0530

    Add files via upload

---
+++ DSA15 (2).java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class DSA16
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		int num1 =10; 
+		int num2 =20;
+		int num3 =30;
+
+		//num1 = num2;
+		//num2 = num3;
+		//num3 = num1;
+		num2 = num2 + num3;
+		num3 = num2 - num3;
+		num2 = num2 - num3;
+
+
+		System.out.println(num2 +"  "+ num3);
+	}
+}
+
+
