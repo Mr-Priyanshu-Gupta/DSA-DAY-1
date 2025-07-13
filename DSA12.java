2a8d47cbc925adf9e8aa6edd6915e040e499fd56
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 17:08:01 +0530

    Add files via upload

---
+++ DSA12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class DSA12
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number 1:");
+ 		double num1 = sc.nextDouble();
+		System.out.println("Enter number 2:");
+ 		double num2 = sc.nextDouble();
+		System.out.println("Enter number 3:");
+ 		double num3 = sc.nextDouble();
+	System.out.println("AVG of Number is : " + ":" + ((num1+num2+num3)/3) );
+		
+	}
+}
+
+
