00dec62f6100e5b28c35b8e24a6d3992ded0b913
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 17:30:18 +0530

    Add files via upload

---
+++ DSA16.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class DSA16
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+	System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+	}
+}
+
+
