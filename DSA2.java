83f9ef98284110f55752d32bab3ec137ddcffe28
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 14:09:20 +0530

    Add files via upload

---
+++ DSA2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class DSA2
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number");
+ 		int num1 = sc.nextInt();
+	 	System.out.println("number 1 is :"+num1);
+		System.out.println("Enter 2nd your number");
+		int num2 = sc.nextInt();
+		System.out.println("number 2 is :" + num2);
+		int Sum = num1 + num2;
+		System.out.println("Sum :" + Sum);
+		
+		
+	}
+}
+
