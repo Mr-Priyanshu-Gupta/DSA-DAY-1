c3445d5ff85f8eb72868fc98ce17f9c91ccd3052
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-13 17:00:03 +0530

    Add files via upload

---
+++ DSA11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class DSA11
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter radius number");
+ 		double num1 = sc.nextDouble();
+		double area = 3.14*num1*num1;
+		System.out.println(area);
+	}
+}
+
+
+
